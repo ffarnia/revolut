4b86da3a4ebf93102bfb5555f1a38d22919046c9
Fazel Farnia <fazel.farnia@gmail.com>
2019-12-11 15:13:35 +0330

    Modify repository

--- src/main/java/com/revolut/challenge/repository/InMemoryRepository.java
+++ src/main/java/com/revolut/challenge/repository/InMemoryRepository.java
@@ -5,20 +5,25 @@ import com.revolut.challenge.model.Account;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * Created by Fazel on 12/11/2019.
  */
 public class InMemoryRepository {
 
-    private static final Map<Integer, Account> repository = new ConcurrentHashMap();
+    private final Map<Integer, Account> repository;
+
+    public InMemoryRepository() {
+        repository = new ConcurrentHashMap();
+    }
 
     public void createAccount(Account account) {
         repository.put(account.getAccountNumber(), account);
     }
 
     public List<Account> loadAllAccounts() {
-        return (List<Account>) repository.values();
+        return repository.values().stream().collect(Collectors.toList());
     }
 
     public Account loadByAccountNumber(Integer accountNumber) {
