156363604c9ed002df93cd7a3f50e567dc03a0c7
Fazel Farnia <fazel.farnia@gmail.com>
2019-12-11 12:02:45 +0330

    Add models

---
+++ src/main/java/com/revolut/challenge/model/Account.java
@@ -0,0 +1,12 @@
+package com.revolut.challenge.model;
+
+/**
+ * Created by Fazel on 12/11/2019.
+ */
+public class Account {
+
+    private Integer accountNumber;
+    private String ownerName;
+    private Integer balance;
+
+}
---
+++ src/main/java/com/revolut/challenge/model/Transaction.java
@@ -0,0 +1,11 @@
+package com.revolut.challenge.model;
+
+/**
+ * Created by Fazel on 12/11/2019.
+ */
+public class Transaction {
+
+    private Integer fromAccountNumber;
+    private Integer toAccountNumber;
+    private Integer amount;
+}
