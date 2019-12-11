7ee74871d094b028cd9f2ccba4f14c2b59fcba7d
Fazel Farnia <fazel.farnia@gmail.com>
2019-12-11 11:51:32 +0330

    Create config files

---
+++ src/main/java/com/revolut/challenge/config/RevolutApplication.java
@@ -0,0 +1,11 @@
+package com.revolut.challenge.config;
+
+/**
+ * Created by Fazel on 12/11/2019.
+ */
+public class RevolutApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/revolut/challenge/config/RevolutConfig.java
@@ -0,0 +1,7 @@
+package com.revolut.challenge.config;
+
+/**
+ * Created by Fazel on 12/11/2019.
+ */
+public class RevolutConfig {
+}
